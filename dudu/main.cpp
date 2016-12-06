cf5e128f76292d2a3a241f2620bf8590f4f645ef
Juscelino Tanaka <juscelino.tanaka@gmail.com>
2016-12-06 12:02:40 -0400

    Initial Commit

---
+++ dudu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dudu
+//
+//  Created by Juscelino Tanaka on 12/6/16.
+//  Copyright © 2016 Juscelino Tanaka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
